6ff688e70c8633cc877a4694e98238c867ba34d5
JonLuca DeCaro <jdecaro@usc.edu>
2017-03-19 13:43:46 -0700

    Images, scrolling code

--- content_script.js
+++ content_script.js
@@ -1,13 +1,48 @@
+addScores();
+var count = 0;
+var scrollTimer,
+    lastScrollFireTime = 0;
+
+$(document).scroll(function() {
+
+    var minScrollTime = 100;
+    var now = new Date().getTime();
+
+    if (!scrollTimer) {
+        if (now - lastScrollFireTime > (3 * minScrollTime)) {
+            var newCount = $(".review").length;
+            if (newCount != count) {
+                addScores();
+            }
+            lastScrollFireTime = now;
+        }
+        scrollTimer = setTimeout(function() {
+            scrollTimer = null;
+            lastScrollFireTime = new Date().getTime();
+            addScores();
+        }, minScrollTime);
+    }
 
 
-$(".review").each(function(index) {
-    var link = $(this).children(".album-link");
-    var title = $(this).find(".album-artist");
-    $.get($(link[0]).attr('href'), function(data) {
-        var rating = parseRating(data);
-        $(title).append("<h2 class=\"title addedScore\">Score: " + rating + "</h2>");
-    });
 });
+
+
+function addScores() {
+    count = $(".review").length;
+    $(".review").each(function(index) {
+        var link = $(this).children(".album-link");
+        var title = $(this).find(".album-artist");
+        if (!$(this).hasClass("addedScore")) {
+            $(this).addClass("addedScore");
+            $.get($(link[0]).attr('href'), function(data) {
+                var rating = parseRating(data);
+                $(title).append("<h2 class=\"title\" id=\"addedScore\">Score: " + rating + "</h2>");
+            });
+        }
+    });
+}
+
+
 
 // for (album in albums) {
 //     console.log(album.querySelector("a"));
